a0d81d3e3a7fea38995704679c6350f88f1566ce
George Lemon <georgelemon@protonmail.com>
2021-09-03 16:11:48 +0300

    Add bin directory, env.yml config and main application file
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- src/app.nim
+++ src/app.nim
@@ -1,5 +1,20 @@
-# This is just an example to get you started. A typical binary package
-# uses this file as the main entry point of the application.
+import supranim
+include ./routes
 
-when isMainModule:
-  echo("Hello, World!")
+Application(
+    # Server address and port number
+    # (Default 127.0.0.1:3399)
+    address: "127.0.0.1",
+    port: 3399.Port,
+    # Boot your app under SSL connection.
+    # If set true, it will automatically generate a self-signed
+    # SSL certificate (in case it does not exist)
+    ssl: true,
+    # Enable multi threading support for your Supranim,
+    # by allocating one or more from available threads.
+    threads: 2,
+    # Relative path to your assets directory
+    # Used by Assets Proxy Handler for routing
+    # your assets to public network
+    assets: "static"
+).start()
