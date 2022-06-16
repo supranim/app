d735be6f65bb137f3e2963c2373c5c2563447a36
George Lemon <georgelemon@protonmail.com>
2022-06-17 01:21:52 +0300

    Update routes
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- src/routes.nim
+++ src/routes.nim
@@ -1,5 +1,12 @@
 import supranim/router
-import controller/pages
+import controller/[pages, err]
+import middleware/auth
 
 # A simple GET route
-Router.get("/", homepage)
+Router.get("/")
+
+# A simple GET route with `authentication` middleware
+Router.get("/profile").middleware(authentication)
+
+Router.setErrorPage(HttpCode(404), err.e404)
+Router.setErrorPage(HttpCode(404), err.e500)
