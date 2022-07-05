36ad326ed9e039093abbc250202fae74786ce5e9
George Lemon <georgelemon@protonmail.com>
2022-07-05 14:04:11 +0300

    Add a blank route
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- src/routes.nim
+++ src/routes.nim
@@ -8,7 +8,7 @@ Router.get("/")
 # A simple GET route protected by middleware
 Router.get("/account").middleware(authentication)
 
-Router.get("/auth")
+Router.get("/blank")
 
 Router.setErrorPage(HttpCode(404), err.e404)
 Router.setErrorPage(HttpCode(404), err.e500)
