d80c202b0bb677eb4ca94fcbcbb7a7bf322b167c
George Lemon <georgelemon@protonmail.com>
2022-07-17 11:30:25 +0300

    update
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- src/events/listeners/system.nim
+++ src/events/listeners/system.nim
@@ -1,7 +1,11 @@
 from std/strutils import indent, `%`
+import supranim/router
 
 Event.listen("system.boot.services") do(services: varargs[Arg]):
-    let timTemplates = Tim.precompile()
+    proc refresh() =
+        when not defined release:
+            {.gcsafe.}: Router.refresh()
+    let timTemplates = Tim.precompile do(): refresh()
     echo indent("✓ Tim Templates", 2)
     for k, timTemplate in timTemplates.pairs():
         let count = k + 1
