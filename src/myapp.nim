b084bcf4ea1a2747a98f4f199cde3166db85d9f8
George Lemon <georgelemon@protonmail.com>
2022-08-05 01:59:14 +0300

    cleanup
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- src/myapp.nim
+++ src/myapp.nim
@@ -7,12 +7,3 @@ App.init()
 
 # Start the application server
 App.start()
-
-
-# from std/os import sleep
-# import supranim/support/finder
-
-
-# var find = newFinder()
-# let results = find.ext(CSS).get()
-# echo results
