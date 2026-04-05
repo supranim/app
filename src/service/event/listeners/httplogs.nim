cec119485cfd7f20c2d989444155fe85c65bd4ef
George Lemon <georgelemon@protonmail.com>
2026-04-05 03:41:25 +0300

    cleanup
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- src/service/event/listeners/httplogs.nim
+++
@@ -1,9 +0,0 @@
-import std/[options, times, logging, strutils]
-
-import supranim/service/events
-import supranim/service/logger
-
-listener "http.error":
-  {.gcsafe.}:
-    let x = args.get()
-    httpLogger.log(lvlError, "$1 [$2] $3" % [x[1], $now(), x[0]])
